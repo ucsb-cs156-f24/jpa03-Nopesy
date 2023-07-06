de197db62dfc10b4c80ac103663618e125c9cc56
Phill Conrad <phtcon@ucsb.edu>
2023-07-06 15:37:11 -0700

    pc - fix eslint errors

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,15 +3,6 @@ import HomePage from "main/pages/HomePage";
 import ProfilePage from "main/pages/ProfilePage";
 import AdminUsersPage from "main/pages/AdminUsersPage";
 
-import TodosIndexPage from "main/pages/Todos/TodosIndexPage";
-import TodosCreatePage from "main/pages/Todos/TodosCreatePage";
-import TodosEditPage from "main/pages/Todos/TodosEditPage";
-
-import UCSBDatesIndexPage from "main/pages/UCSBDates/UCSBDatesIndexPage";
-import UCSBDatesCreatePage from "main/pages/UCSBDates/UCSBDatesCreatePage";
-import UCSBDatesEditPage from "main/pages/UCSBDates/UCSBDatesEditPage";
-
-
 import { hasRole, useCurrentUser } from "main/utils/currentUser";
 
 import "bootstrap/dist/css/bootstrap.css";
@@ -29,32 +20,6 @@ function App() {
         {
           hasRole(currentUser, "ROLE_ADMIN") && <Route exact path="/admin/users" element={<AdminUsersPage />} />
         }
-        {
-          hasRole(currentUser, "ROLE_USER") && (
-            <>
-              <Route exact path="/todos/list" element={<TodosIndexPage />} />
-              <Route exact path="/todos/create" element={<TodosCreatePage />} />
-              <Route exact path="/todos/edit/:todoId" element={<TodosEditPage />} />
-            </>
-          )
-        }
-
-        {
-          hasRole(currentUser, "ROLE_USER") && (
-            <>
-              <Route exact path="/ucsbdates/list" element={<UCSBDatesIndexPage />} />
-            </>
-          )
-        }
-        {
-          hasRole(currentUser, "ROLE_ADMIN") && (
-            <>
-              <Route exact path="/ucsbdates/edit/:id" element={<UCSBDatesEditPage />} />
-              <Route exact path="/ucsbdates/create" element={<UCSBDatesCreatePage />} />
-            </>
-          )
-        }
-
       </Routes>
     </BrowserRouter>
   );
--- frontend/src/tests/components/Users/UsersTable.test.js
+++ frontend/src/tests/components/Users/UsersTable.test.js
@@ -1,4 +1,4 @@
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import usersFixtures from "fixtures/usersFixtures";
 import UsersTable from "main/components/Users/UsersTable"
 
@@ -18,29 +18,29 @@ describe("UserTable tests", () => {
     });
 
     test("Has the expected colum headers and content", () => {
-        const { getByText, getByTestId } = render(
-          <UsersTable users={usersFixtures.threeUsers}/>
+        render(
+            <UsersTable users={usersFixtures.threeUsers} />
         );
-    
+
         const expectedHeaders = ["id", "First Name", "Last Name", "Email", "Admin"];
         const expectedFields = ["id", "givenName", "familyName", "email", "admin"];
         const testId = "UsersTable";
 
-        expectedHeaders.forEach( (headerText)=> {
-            const header = getByText(headerText);
+        expectedHeaders.forEach((headerText) => {
+            const header = screen.getByText(headerText);
             expect(header).toBeInTheDocument();
         });
 
-        expectedFields.forEach( (field)=> {
-          const header = getByTestId(`${testId}-cell-row-0-col-${field}`);
-          expect(header).toBeInTheDocument();
+        expectedFields.forEach((field) => {
+            const header = screen.getByTestId(`${testId}-cell-row-0-col-${field}`);
+            expect(header).toBeInTheDocument();
         });
 
-        expect(getByTestId(`${testId}-cell-row-0-col-id`)).toHaveTextContent("1");
-        expect(getByTestId(`${testId}-cell-row-0-col-admin`)).toHaveTextContent("true");
-        expect(getByTestId(`${testId}-cell-row-1-col-id`)).toHaveTextContent("2");
-        expect(getByTestId(`${testId}-cell-row-1-col-admin`)).toHaveTextContent("false");
+        expect(screen.getByTestId(`${testId}-cell-row-0-col-id`)).toHaveTextContent("1");
+        expect(screen.getByTestId(`${testId}-cell-row-0-col-admin`)).toHaveTextContent("true");
+        expect(screen.getByTestId(`${testId}-cell-row-1-col-id`)).toHaveTextContent("2");
+        expect(screen.getByTestId(`${testId}-cell-row-1-col-admin`)).toHaveTextContent("false");
 
-      });
+    });
 });
 
