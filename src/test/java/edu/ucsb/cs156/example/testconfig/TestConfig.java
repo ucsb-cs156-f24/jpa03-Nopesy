39b03453e45a7d275688d475915168770d670d52
Daniel Jensen <djensen2@outlook.com>
2024-10-10 18:53:03 -0700

    dj - added import of securityconfig

--- src/test/java/edu/ucsb/cs156/example/testconfig/TestConfig.java
+++ src/test/java/edu/ucsb/cs156/example/testconfig/TestConfig.java
@@ -1,12 +1,15 @@
 package edu.ucsb.cs156.example.testconfig;
 
+import edu.ucsb.cs156.example.config.SecurityConfig;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
 
 import edu.ucsb.cs156.example.services.CurrentUserService;
 import edu.ucsb.cs156.example.services.GrantedAuthoritiesService;
+import org.springframework.context.annotation.Import;
 
 @TestConfiguration
+@Import(SecurityConfig.class)
 public class TestConfig {
 
     @Bean
